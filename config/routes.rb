eb46a8dfc1267df81697a950ed8f862581673116
Artyom <artemk@svitla.com>
2010-12-03 15:26:51 +0200

    test

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :users, :only => [],  :shallow => true do |user|
     user.resources :blog_posts, :collection => {:drafts => :any}, :member => {:tag => :any}, :has_many => :blog_comments
   end
+  
 end
